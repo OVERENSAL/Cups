63753ea334ab722a44d1776069338e7e8979b687
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2021-04-26 13:51:34 +0300

    added second scene

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
     public void click() {
     	UnityEngine.Debug.Log("click");
+        SceneManager.LoadScene(sceneName: "LevelChoosing");
     }
 }
